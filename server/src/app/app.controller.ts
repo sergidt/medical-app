d9b2fcc5201fe5bdc7dbf9aad84f49b7c4dab617
Sergi Dote <sergi.dote@bizerba.com>
2025-06-24 10:33:41 +0200

    add patients service

--- server/src/app/app.controller.ts
+++ server/src/app/app.controller.ts
@@ -3,11 +3,11 @@ import { PatientsService } from './patients.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly patientsList: PatientsService) {
+    constructor(private readonly patientService: PatientsService) {
     }
 
     @Get()
     getPatientsList() {
-        return this.patientsList.getData();
+        return this.patientService.getPatientsList();
     }
 }
